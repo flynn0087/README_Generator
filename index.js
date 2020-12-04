5061ea8dcd8c3b3d3d6f33d9fadce1ba509970ae
Ben Flynn <flynn0087@gmail.com>
2020-12-04 12:38:14 -0600

    added q1 and required util

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function userInput = () =>
+    inquirer.prompt([
+        {
+            type: "input"
+            name: "title"
+            message: "What is the title of your project"
+        },
+    ])
